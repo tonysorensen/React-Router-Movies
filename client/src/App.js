d196b725653086b48b5fbd59b70e1ed57808a14b
Tony Sorensen <tony48853@gmail.com>
2020-08-05 19:13:09 -0700

    Home button link working. Detail cards working

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import SavedList from "./Movies/SavedList";
-import { Link, Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
-console.log(movieList)
+  console.log(movieList);
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -27,6 +27,7 @@ console.log(movieList)
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
+
   return (
     <div>
       <SavedList
@@ -36,19 +37,16 @@ console.log(movieList)
           ]
         }
       />
-      <div className="buttons">
-        <button className="navbutton">
-          <Link to="/">Home</Link>
-        </button>
-        <button className="navbutton">
-          <Link to="/movies">Movies</Link>
-        </button>
-        <Route path="/" render={() => {
-          return <MovieList movies={movieList}/>
-        }}/>
-    
-        <Route path="/movies/:id" component ={Movie}>
-        </Route>
+      <div>
+        <Switch>
+          <Route path="/movies/:id" component={Movie}></Route>
+          <Route
+            path="/"
+            render={() => {
+              return <MovieList movies={movieList} />;
+            }}
+          />
+        </Switch>
       </div>
     </div>
   );
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {useParams} from 'react-router-dom'
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  const params = useParams();
+  console.log('params',params)
   useEffect(() => {
-    const id = props.id;
+    const id = params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,13 +14,12 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
-          console.log("Hello",response.data)
-        }, [props.id])
+        })
         .catch(error => {
           console.error(error);
         });
 
-  },[props.id]);
+  },[params.id]);
    
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => {
