6f007eee62455b929c702ec59118f255e71a58a8
Tony Sorensen <tony48853@gmail.com>
2020-08-05 17:06:34 -0700

    Router imported and wrapping App in index.js. Routes and Links setup in app.js. MovieList and Movie imported to app.js. Buttons added for links and styled, Switched to Tony-Sorensen branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 import { Link, Route } from "react-router-dom";
-import MovieList from './Movies/MovieList'
-import Movie from './Movies/Movie'
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
@@ -12,29 +12,39 @@ const App = () => {
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
+        .get("http://localhost:5000/api/movies")
+        .then((response) => {
           setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
       <div className="buttons">
-      <button className="navbutton"><Link to="/">Home</Link></button>
-          <button className="navbutton"><Link to="/movies">Movies</Link></button>
-      <Route path="/"></Route>
-      <Route path ="/movies/:id"></Route>
+        <button className="navbutton">
+          <Link to="/">Home</Link>
+        </button>ga
+        <button className="navbutton">
+          <Link to="/movies">Movies</Link>
+        </button>
+        <Route path="/"></Route>
+        <Route path="/movies/:id"></Route>
       </div>
     </div>
   );
