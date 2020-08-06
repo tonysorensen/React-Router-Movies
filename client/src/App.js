4b997ec75e4b66083cb2429f7bfea753e94ec3fe
Tony Sorensen <tony48853@gmail.com>
2020-08-06 09:53:48 -0700

    Added lots of notes to components to explain when state was set and what it was set to, what props were being passed and used, and explaining how functions were operating.

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,27 @@ import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+
+
+  //Setting state here. setMovieList is pulling the data from server.js in the main file which also holds this client file. response.data===movies, the array of objects in server.js. useState is setting up an empty array to hold that data. Below the consol logs show first the empty array created with useState, next that array as it's populated with the data from the get request. The ones below that show the props that are recieved when that data is passed to the MoveList component. The final one is the same as the initial data, just showing it after it has been set to State as setMovieList(response.data) in App.js in the .then portion of the get request. The way the object is set up in server.js it only returns id, title, director, metascore when a request is sent to  http://localhost:5000/api/movies, when a request is sent to http://localhost:5000/api/movies/id it will return the entire object which includes an array of stars(See Movie component request)
+// App.js:22 movielist []
+// App.js:22 movielist (6) [{…}, {…}, {…}, {…}, {…}, {…}]
+// MovieList.js:16 movie details {id: 0, title: "The Godfather", director: "Francis Ford Coppola", metascore: 100}
+// MovieList.js:16 movie details {id: 1, title: "Star Wars", director: "George Lucas", metascore: 92}
+// MovieList.js:16 movie details {id: 2, title: "The Lord of the Rings: The Fellowship of the Ring", director: "Peter Jackson", metascore: 92}
+// MovieList.js:16 movie details {id: 3, title: "Terminator 2: Judgement Day", director: "James Cameron", metascore: 94}
+// MovieList.js:16 movie details {id: 4, title: "Dumb and Dumber", director: "The Farely Brothers", metascore: 76}
+// MovieList.js:16 movie details {id: 5, title: "Tombstone", director: "George P. Cosmatos", metascore: 89}
+// App.js:29 response data (6) [{…}, {…}, {…}, {…}, {…}, {…}]
   const [movieList, setMovieList] = useState([]);
-  console.log(movieList);
+  console.log('movielist', movieList);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get("http://localhost:5000/api/movies")
+        .get("http://localhost:5000/api/movies/")
         .then((response) => {
           setMovieList(response.data);
+          console.log('response data', response.data)
         })
         .catch((error) => {
           console.error("Server Error", error);
@@ -38,7 +51,9 @@ const App = () => {
       />
       <div>
         <Switch>
+        {/* Here the route is set for the movie component with a dynamic id. No props are being set here. The data is received and props set in the Movie component*/}
           <Route path="/movies/:id" component={Movie}></Route>
+          {/* This Route is for the MovieList which appears on the home page. The data is State here but is passed as Props to the MovieList component. Yes, state is passed as props.  */}
           <Route
             path="/"
             render={() => {
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import {useParams} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+//Props are being passed from App.js but those props lack the stars that are displayed on the Movie card and we can't change props once they are received. If we had received ALL of the data in our App.js this would be easy. Instead, we have to make a new call here with ${id} to get the stars info we need. Below we see the console log when we use useParams. We set useParams to the variable name params at the point of the consol log params has grabbed the id of the card number we clicked on in the MovieList component. By the end of the useEffect it has called for the full information from the card with the given id. Then "movie" is set to the props we want from that request(all of them) and a card is returned with the info.
+// Moviw.js:12 params {id: "1"}
+// Movie.js:20 movie card response {id: 1, title: "Star Wars", director: "George Lucas", metascore: 92, stars: Array(3)}
 const Movie = (props) => {
   const [movie, setMovie] = useState();
   const params = useParams();
-  console.log('params',params)
+  console.log("params", params);
+  // debugger;
   useEffect(() => {
     const id = params.id;
+    console.log(params.id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-          console.log('response',response.data)
-        })
-        .catch(error => {
-          console.error(error);
-        });
-
-  },[params.id]);
-   
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then((response) => {
+        setMovie(response.data);
+        console.log("movie card response", response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [params.id]);
+  // debugger;
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => {
   // }
@@ -29,7 +34,7 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
+//Here we are setting the props we want from movie which is the state variable we set above. Since we used setMovie to populate movie(which was empty until then), we have access to the response.data from our get request. Our response.data contains title, director, metascore, and an array of stars.
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
@@ -42,8 +47,8 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
-        {stars.map(star => (
+{/* Here we are mapping over the stars array contained in thge object we called using its id. We create a div with each star's name. */}
+        {stars.map((star) => (
           <div key={star} className="movie-star">
             {star}
           </div>
@@ -52,6 +57,6 @@ const Movie = (props) => {
       <div className="save-button">Save</div>
     </div>
   );
-}
+};
 
 export default Movie;
